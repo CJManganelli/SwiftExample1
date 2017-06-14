033ca5a5b7c96d6d8890b0c897df77c781c02603
Carl Manganelli <carl@Carls-MacBook-Pro-3.local>
2017-06-14 11:19:32 -0500

    first commit, declared the first constant

---
+++ SwiftExample1/SwiftExample1/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SwiftExample1
+//
+//  Created by Carl Manganelli on 6/14/17.
+//  Copyright © 2017 Carl Manganelli. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let sample1: UInt8 = 0x3A
+
+
+
